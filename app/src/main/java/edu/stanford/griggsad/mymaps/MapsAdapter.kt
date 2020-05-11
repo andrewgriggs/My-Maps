e7bf4a3741f568efce9383e9271db582ae22f479
Andrew <griggsad@stanford.edu>
2020-05-10 22:55:08 -0700

    Initial commit

---
+++ app/src/main/java/edu/stanford/griggsad/mymaps/MapsAdapter.kt
@@ -0,0 +1,39 @@
+package edu.stanford.griggsad.mymaps
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import edu.stanford.griggsad.mymaps.models.UserMap
+
+private const val TAG = "MapsAdapter"
+class MapsAdapter(val context: Context, val userMaps: List<UserMap>, val onClickListener: OnClickListener) : RecyclerView.Adapter<MapsAdapter.ViewHolder>() {
+
+    interface OnClickListener {
+        fun onItemClick(position: Int)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_user_map, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = userMaps.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val userMap = userMaps[position]
+        holder.itemView.setOnClickListener {
+            Log.i(TAG, "Tapped on $position")
+            onClickListener.onItemClick(position)
+        }
+        val textViewTitle = holder.itemView.findViewById<TextView>(R.id.tvMapTitle)
+        val textViewCount = holder.itemView.findViewById<TextView>(R.id.tvLocationCount)
+        textViewTitle.text = userMap.title
+        textViewCount.text = "Locations: " + userMap.places.size.toString()
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
